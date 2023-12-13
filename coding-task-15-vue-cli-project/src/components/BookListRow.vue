3e30f84a9f18262041bf5fc5744b2f7b424dee89
Marek Pentzek <marekpj96@gmail.com>
2023-12-13 13:37:32 +0100

    everything new

---
+++ coding-task-15-vue-cli-project/src/components/BookListRow.vue
@@ -0,0 +1,51 @@
+<template>
+  <tr class=".table-item__table td">
+    <td>{{ title }}</td>
+    <td>{{ isbn }}</td>
+    <td>
+      <BaseButton
+        name="actonCol"
+        :btntext="btntext"
+        :variant="variant"
+        @press="handleAddBookmark"
+      />
+    </td>
+  </tr>
+</template>
+
+<script>
+import BaseButton from "@/components/BaseButton.vue";
+
+export default {
+  props: {
+    title: String,
+    isbn: String,
+  },
+  components: {
+    BaseButton,
+  },
+  data() {
+    return {
+      btntext: "Add Bookmark",
+      variant: true,
+      isBookmarked: false,
+    };
+  },
+  methods: {
+    handleAddBookmark() {
+      this.isBookmarked = !this.isBookmarked;
+      if (this.btntext === "Add Bookmark") {
+        this.btntext = "Remove Bookmark";
+      } else {
+        this.btntext = "Add Bookmark";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.table-item__table td {
+  padding: 12px 15px;
+}
+</style>
